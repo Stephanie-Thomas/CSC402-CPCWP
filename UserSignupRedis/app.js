1a48fb5909120002dc3c6b9d0da907e2c629f463
kmatotek <kadinstitch@gmail.com>
2025-02-19 18:55:49 -0500

    nodemon avail

--- UserSignupRedis/app.js
+++ UserSignupRedis/app.js
@@ -1,31 +1,12 @@
+// app.js
 import express from 'express';
-import session from 'express-session';
-import Redis from 'ioredis';
-import RedisStore from 'connect-redis';
+import sessionMiddleware from './sessionStore.js';
 
 const app = express();
 const port = 3000;
 
-// Initialize Redis client
-const redisClient = new Redis();
-
-// Configure session middleware
-app.use(
-  session({
-    store: new RedisStore({
-      client: redisClient,  // Pass the Redis client instance
-      disableTouch: true,   // Disable automatic "touching" of sessions
-    }),
-    secret: 'super-secret-key',  // Secret key for signing session ID cookie
-    resave: false,               // Prevents unnecessary session saving
-    saveUninitialized: false,    // Avoid saving empty sessions
-    cookie: {
-      httpOnly: true,  // Prevents JavaScript access to cookies
-      secure: false,   // Set `true` in production (requires HTTPS)
-      maxAge: 10000, // Session expiration time in milliseconds (1 hour)
-    },
-  })
-);
+// Use the session middleware
+app.use(sessionMiddleware);
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -40,7 +21,6 @@ app.post('/login', (req, res) => {
 
   // Store user data in session
   req.session.user = { username, userId: 1 };
-
   res.send('Login Successful');
 });
 
@@ -60,7 +40,6 @@ app.get('/home', (req, res) => {
   if (!req.session.user) {
     return res.status(401).send('Invalid session');
   }
-
   res.send({ sessionData: req.session.user });
 });
 
