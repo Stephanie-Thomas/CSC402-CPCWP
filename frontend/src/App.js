d1699fc059d66df76864a5b58a48d00d8ca2a32a
kmatotek <kadinstitch@gmail.com>
2025-03-07 22:02:19 -0500

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LCLeaderboard from './components/LCLeaderboard';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>LeetCode Stats Viewer</h1>
+      <LCLeaderboard/>
+    </div>
+  );
+};
+
+export default App;
