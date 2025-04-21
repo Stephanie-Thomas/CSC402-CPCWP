88a1e38ade03d0844797cb45103a192e423fd76d
tobibussiek <143556814+tobibussiek@users.noreply.github.com>
2025-04-21 18:55:51 -0400

    Changed Port in server.js to pass test

--- backend-node/server.js
+++ backend-node/server.js
@@ -9,7 +9,7 @@ dotenv.config(); // Load environment variables
 const apiRoutes = require('./routes/api.js');
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 4000;
 
 // Connect to MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI, {
@@ -26,7 +26,7 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 // Middleware
-app.use(cors({ origin: 'https://csc402-cpcwp.onrender.com' }));
+app.use(cors({ origin: CLIENT_ORIGIN }));
 app.use(express.json());
 app.use('/api', apiRoutes);
 
