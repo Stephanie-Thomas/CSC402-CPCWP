d29aeaaf2ca0c23872edc5df94a40b0bcd80e005
tobibussiek <143556814+tobibussiek@users.noreply.github.com>
2025-04-22 17:44:55 -0400

    updated usrmodel.js as well

--- backend-node/models/userModel.js
+++ backend-node/models/userModel.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 //Defines fields and their types for user documents in mongoDB
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    leetcodeUsername: { type: String, required: true },
-    codeforcesUsername: { type: String, required: true }
-  }, { timestamps: true });
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  leetcodeUsername: { type: String, required: false, default: null },
+  codeforcesUsername: { type: String, required: false, default: null }
+}, { timestamps: true });
 
 //Lets you query and manipulate the users within MongoDB whenever require("User") is used in code
 module.exports = mongoose.model("User", userSchema);
